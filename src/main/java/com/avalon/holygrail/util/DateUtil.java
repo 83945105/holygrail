ceb177e84de036649d841fa15ef4cd6f48b1547d
wx_83945105 <18606167809@163.com>
2018-03-19 10:03:19 +0800

    新增时间工具类

---
+++ src/main/java/com/avalon/holygrail/util/DateUtil.java
@@ -0,0 +1,36 @@
+package com.avalon.holygrail.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+
+    private final static SimpleDateFormat sdfYear = new SimpleDateFormat("yyyy");
+    private final static SimpleDateFormat sdfDay = new SimpleDateFormat("yyyy-MM-dd");
+    private final static SimpleDateFormat sdfDays = new SimpleDateFormat("yyyyMMdd");
+    private final static SimpleDateFormat sdfTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+
+    /**
+     * 获取YYYY-MM-DD HH:mm:ss格式
+     *
+     * @return
+     */
+    public static String getTimeString() {
+        return sdfTime.format(new Date());
+    }
+
+    /**
+     * 获取时间戳
+     *
+     * @return
+     */
+    public static Long getTimeStamp() {
+        return new Date().getTime();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(DateUtil.getTimeStamp());
+        System.out.println(DateUtil.getTimeString());
+    }
+}
