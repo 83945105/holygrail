bef2c0bdc6387174534b938b606b557f87c4857f
wx_83945105 <18606167809@163.com>
2018-03-22 17:48:08 +0800

    日常提交

---
+++ src/main/java/com/avalon/holygrail/enums/Status.java
@@ -0,0 +1,17 @@
+package com.avalon.holygrail.enums;
+
+/**
+ * 状态枚举
+ * Created by 白超 on 2018/3/21.
+ */
+public enum Status {
+
+    /**正常*/
+    NORMAL,
+    /**启用*/
+    ENABLED,
+    /**禁用*/
+    DISABLED,
+    /**删除*/
+    DELETED
+}
