dd1ba0fad47a798dbd79a262fbb8f779c5f3b263
83945105 <18606167809@163.com>
2018-03-31 21:38:13 +0800

    HttpUtil新增一个请求方法

--- src/main/java/com/avalon/holygrail/util/HttpUtil.java
+++ src/main/java/com/avalon/holygrail/util/HttpUtil.java
@@ -1,6 +1,14 @@
 package com.avalon.holygrail.util;
 
+import net.sf.json.JSONObject;
+
+import javax.net.ssl.*;
 import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.net.ConnectException;
+import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 /**
  * Http工具
@@ -8,12 +16,118 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class HttpUtil {
 
+    enum MethodType {
+        GET, POST, HEAD, OPTIONS, PUT, DELETE, TRACE
+    }
+
     /**
      * 判断是否是AJAX请求
+     *
      * @param request 请求
      * @return
      */
     public static boolean isAjax(HttpServletRequest request) {
         return "XMLHttpRequest".equals(request.getHeader("X-Requested-With"));
+    }
+
+    /**
+     * 发起https请求并获取结果
+     *
+     * @param requestUrl    请求地址
+     * @param methodType    请求方式
+     * @param outputStr     提交的数据
+     * @return JSONObject(通过JSONObject.get ( key)的方式获取json对象的属性值)
+     */
+    public static JSONObject httpRequest(String requestUrl, MethodType methodType, String outputStr) {
+        JSONObject jsonObject = null;
+        StringBuffer buffer = new StringBuffer();
+        // 创建SSLContext对象，并使用我们指定的信任管理器初始化
+        TrustManager[] tm = {new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        }};
+
+        SSLContext sslContext;
+        URL url;
+        OutputStream outputStream;
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader bufferedReader = null;
+        HttpsURLConnection httpUrlConn = null;
+
+        try {
+            sslContext = SSLContext.getInstance("SSL", "SunJSSE");
+            sslContext.init(null, tm, new java.security.SecureRandom());
+            // 从上述SSLContext对象中得到SSLSocketFactory对象
+            SSLSocketFactory ssf = sslContext.getSocketFactory();
+
+            url = new URL(requestUrl);
+            httpUrlConn = (HttpsURLConnection) url.openConnection();
+            httpUrlConn.setSSLSocketFactory(ssf);
+
+            httpUrlConn.setDoOutput(true);
+            httpUrlConn.setDoInput(true);
+            httpUrlConn.setUseCaches(false);
+            // 设置请求方式（GET/POST）
+            httpUrlConn.setRequestMethod(methodType.name());
+
+            if ("GET".equalsIgnoreCase(methodType.name()))
+                httpUrlConn.connect();
+
+            // 当有数据需要提交时
+            if (null != outputStr) {
+                outputStream = httpUrlConn.getOutputStream();
+                // 注意编码格式，防止中文乱码
+                outputStream.write(outputStr.getBytes("UTF-8"));
+                outputStream.close();
+            }
+
+            // 将返回的输入流转换成字符串
+            inputStream = httpUrlConn.getInputStream();
+            inputStreamReader = new InputStreamReader(inputStream, "utf-8");
+            bufferedReader = new BufferedReader(inputStreamReader);
+
+            String str;
+            while ((str = bufferedReader.readLine()) != null) {
+                buffer.append(str);
+            }
+            jsonObject = JSONObject.fromObject(buffer.toString());
+        } catch (ConnectException e) {
+            System.out.println("server connection timed out.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedReader != null) bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (inputStreamReader != null) inputStreamReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (inputStream != null) inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (httpUrlConn != null) httpUrlConn.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return jsonObject;
     }
 }
