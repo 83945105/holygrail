278a15c91f3924ce9c1e7f6d926ed8d86d56bcf7
wx_83945105 <18606167809@163.com>
2018-06-29 14:56:09 +0800

    修复 JsonView新增 getRows() bug

--- src/main/java/com/avalon/holygrail/ss/view/JsonView.java
+++ src/main/java/com/avalon/holygrail/ss/view/JsonView.java
@@ -1,5 +1,6 @@
 package com.avalon.holygrail.ss.view;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.avalon.holygrail.ss.bean.JsonResultInfo;
 import com.avalon.holygrail.ss.norm.Limit;
@@ -7,6 +8,7 @@ import com.avalon.holygrail.ss.norm.ResultInfo;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  * Created by 白超 on 2018/6/3.
@@ -34,11 +36,14 @@ public class JsonView<K, V> extends HashMap<K, V> implements DataView {
 
     public <T> ArrayList<T> getRows(Class<T> clazz) {
         ArrayList<T> rows = new ArrayList<>();
-        JSONObject jsonObject = (JSONObject) this.get(LimitDataView.ROWS_KEY);
-        if (jsonObject == null) {
+        JSONArray jsonArray = (JSONArray) this.get(LimitDataView.ROWS_KEY);
+        if (jsonArray == null) {
             return rows;
         }
-        jsonObject.forEach((s, o) -> rows.add(JSONObject.parseObject(((JSONObject) o).toJSONString(), clazz)));
+        Iterator iterator = jsonArray.iterator();
+        while (iterator.hasNext()) {
+            rows.add(JSONObject.parseObject(((JSONObject) iterator.next()).toJSONString(), clazz));
+        }
         return rows;
     }
 
