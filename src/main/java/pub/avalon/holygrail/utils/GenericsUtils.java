48dafcc54ea1ce33481e3926e2d21d4f784a0865
wx_83945105 <BAI_chao910405>
2019-07-17 17:55:34 +0800

    新增GenericsUtils工具类

--- src/main/java/pub/avalon/holygrail/utils/GenericsUtils.java
+++ src/main/java/pub/avalon/holygrail/utils/GenericsUtils.java
@@ -25,9 +25,6 @@ public class GenericsUtils {
     public static Class getSuperClassGenricType(Class clazz, int index)
             throws IndexOutOfBoundsException {
         Type genType = clazz.getGenericSuperclass();
-        if (genType instanceof Class) {
-            return (Class) genType;
-        }
         if (!(genType instanceof ParameterizedType)) {
             return Object.class;
         }
@@ -39,6 +36,32 @@ public class GenericsUtils {
             return Object.class;
         }
         return (Class) params[index];
+    }
+
+    /**
+     * 从祖先类型中获取期望的泛型参数类型
+     *
+     * @param clazz
+     * @param expectClass
+     * @return
+     */
+    public static Class getExpectAncestorsClassGenricType(Class clazz, Class<?> expectClass) {
+        if (clazz == Object.class) {
+            return null;
+        }
+        Type genType = clazz.getGenericSuperclass();
+        if (genType instanceof ParameterizedType) {
+            Type[] params = ((ParameterizedType) genType).getActualTypeArguments();
+            for (Type param : params) {
+                if (param instanceof Class) {
+                    if (expectClass.isAssignableFrom((Class<?>) param)) {
+                        return (Class) param;
+                    }
+                }
+            }
+
+        }
+        return getExpectAncestorsClassGenricType(clazz.getSuperclass(), expectClass);
     }
 
 }
